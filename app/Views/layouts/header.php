26a8e71d22d4eb3d73ebc6ae1cba4ef8767a122a
cipheraenys <68282629+cipheraenys@users.noreply.github.com>
2020-09-16 16:20:00 +0700

    feat: add main layout header with bootstrap navbar

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= esc($title ?? 'SIM-IPAM Lab SMK') ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <style>
+        body { background-color: #f4f6f9; }
+        .card { border: none; box-shadow: 0 1px 3px rgba(0,0,0,.12); }
+        .navbar-brand i { color: #00d1b2; }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-0">
+    <div class="container">
+        <a class="navbar-brand" href="<?= site_url('/') ?>">
+            <i class="fas fa-network-wired"></i> SIM-IPAM TKJ
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item <?= url_is('/') ? 'active' : '' ?>">
+                    <a class="nav-link" href="<?= site_url('/') ?>"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                </li>
+                <li class="nav-item <?= url_is('ipam*') ? 'active' : '' ?>">
+                    <a class="nav-link" href="<?= site_url('ipam') ?>"><i class="fas fa-list"></i> Data Alokasi IP</a>
+                </li>
+                <li class="nav-item <?= url_is('calculator*') ? 'active' : '' ?>">
+                    <a class="nav-link" href="<?= site_url('calculator') ?>"><i class="fas fa-calculator"></i> Kalkulator Subnet</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container mt-4">
